a821c7789879877d7b3070dcdd91dfea2145fe5f
himanshu <himanshu03vsk@gmail.com>
2021-11-26 22:09:10 +0530

    Feeding input data to model done

--- application.py
+++ application.py
@@ -17,30 +17,30 @@ app = Flask(__name__)
 
 
 # Ensure responses aren't cached
+@app.after_request
+def after_request(response):
+    response.headers["Cache-Control"] = "no-cache, no-store, must-revalidate"
+    response.headers["Expires"] = 0
+    response.headers["Pragma"] = "no-cache"
+    return response
+
+def final_output(price, km_driven,  no_of_owners ,age, diesel, petrol, seller_type_individual,transmission_manual):
+    # importing libraries
+    import pickle
+    from sklearn.preprocessing import StandardScaler
+    input_parameters = [[price, km_driven,  no_of_owners ,age, diesel, petrol, seller_type_individual, transmission_manual]]
+    sc = StandardScaler()
+    import numpy as np
+    ip = np.array(input_parameters)
+    ip.reshape(1,-1)
+    input_parameters_scaled = sc.fit_transform(input_parameters)
+
+    with open('random_forest_regression_model.pkl', 'rb') as file:
+        predictor = pickle.load(file)
+    result = predictor.predict(input_parameters_scaled)
+    return result
 
 
-# Custom filter
-# app.jinja_env.filters["usd"] = usd
-
-# Configure session to use filesystem (instead of signed cookies)
-# app.config["SESSION_FILE_DIR"] = mkdtemp()
-# app.config["SESSION_PERMANENT"] = False
-# app.config["SESSION_TYPE"] = "filesystem"
-# Session(app)
-
-# Configure CS50 Library to use SQLite database
-# db = SQL("sqlite:///finance.db")
-# number_of_users = db.execute("SELECT COUNT() FROM users")
-# number_of_users = number_of_users[0]["COUNT()"]
-# if number_of_users == 0:
-#     number_of_users = 1
-# else:
-#     number_of_users = number_of_users + 1
-
-
-# Make sure API key is set
-# if not os.environ.get("API_KEY"):
-#     raise RuntimeError("API_KEY not set")
 
 
 @app.route("/", methods=["GET", "POST"])
@@ -48,6 +48,51 @@ def index():
     """Show portfolio of stocks"""
     return render_template("index.html")
 
+@app.route("/submit", methods=["GET","POST"])
+def submit():
+    """gets data from user, feeds it into the model to get prediction"""
+    if request.method == "POST":
+        # print(request.form)
+        year = int(request.form.get('year'))
+        age = 2021 - year
+        km_driven = int(request.form.get('km_driven'))
+        price = float(request.form.get('price'))
+        no_of_owners = int(request.form.get('no_of_owners'))
+        petrol = 0
+        diesel = 0
+        transmission = request.form.get('transmission')
+        if transmission == "on":
+            transmission_manual = 0
+        else:
+            transmission_manual = 1
+        seller_type = request.form.get('buyer_kind')
+        if seller_type == 'dealer':
+            seller_type_individual = 0
+        else:
+            seller_type_individual = 1
+        fuel = ['petrol','diesel','cng']
+        if request.form.get(fuel[0]) == 'on':
+            petrol = 1
+            diesel = 0
+            # fuel_type = fuel[0]
+        if request.form.get(fuel[1]) == 'on':
+            petrol = 0
+            diesel = 1
+            # fuel_type = fuel[1]
+        if request.form.get(fuel[2]) == 'on':
+            petrol = 0
+            diesel = 0
+            # fuel_type = fuel[2]
+        output = final_output(price, km_driven,  no_of_owners ,age, diesel, petrol, seller_type_individual, transmission_manual)
+        return f"{output}"
+
+
+    
+     
+        
+
+    # print(request.form["car"])
+    return "<h1>Hello</h1>"
 
 
 
@@ -55,17 +100,4 @@ def index():
 
 
 
-
-
-
-
-# def errorhandler(e):
-#     """Handle error"""
-#     if not isinstance(e, HTTPException):
-#         e = InternalServerError()
-#     return apology(e.name, e.code)
-
-
-# # Listen for errors
-# for code in default_exceptions:
-#     app.errorhandler(code)(errorhandler)
+# ImmutableMultiDict([('year', '2019'), ('price', '14.22'), ('price_in_lakh', '49000'), ('petrol', 'on'), ('switch', 'on')])
